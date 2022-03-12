b8145bb57f5dcb5e9c6fe61a76fd7568878c9cab
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2022-03-12 18:10:20 -0500

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Entry point
- * Return:1
+ * main - Prints quote to the standard error
+ *
+ * Return: zero on success
+ *
  */
 int main(void)
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-write(2, ch, sizeof(ch));
-return (1);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
