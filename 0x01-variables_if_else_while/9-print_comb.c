675c0a6b515aca74cb0a545b5ed4ba781e482179
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2022-03-13 08:56:33 -0400

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: print alphabets using putchar
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}	
+	}
+	putchar('\n');
+	return (0);
+}
