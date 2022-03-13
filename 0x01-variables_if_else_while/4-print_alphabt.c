2ffa57ff7f078c437907e614b7cbe0b1881da0f8
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2022-03-13 06:36:06 -0400

    4-print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: print alphabets using putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char hello[] = "abcdefghijklmnopqrstuvwxyz\n";
+	char *s;
+
+	s = hello;
+
+	while (*s != '\0')
+	{
+		out = putchar(*s);
+		if (out == 'q' || out == 'e')
+			s++;
+			continue;
+		putchar(*s);
+		s++;
+	}
+	return (0);
+}
+
