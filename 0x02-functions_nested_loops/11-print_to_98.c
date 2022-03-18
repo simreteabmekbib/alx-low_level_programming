1677b6693f3d6ba9aa2d5199e6f5bbcaa3267fd2
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2022-03-18 08:19:16 -0400

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print_to_98 - print untill 98
+ * @n: starting point
+ */
+void print_to_98(int n)
+{
+	int i;
+	if (n <= 98)
+	{
+		for (i = n; i <=98; i++)
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+	}	
+}
