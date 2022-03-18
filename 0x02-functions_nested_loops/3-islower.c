e5c072a9b311678e4014b87d315a96ad3e433f93
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2022-03-18 01:30:15 -0400

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,9 @@
 /**
- * _islower - return 1 if lower case alphabet
+ * _islower - returns 1 if char is lowercase
  *
+ * @c: character to be checked if lower case, type int
+ *
+ * Return: 1 if lower case, 0 otherwise
  **/
 
 int _islower(int c)
