8d71a38850d33538e5fbcae3f4c17dc812ac809c
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2022-03-18 02:26:05 -0400

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,12 @@
 /**
  * _abs - gives absolute value of a number
- *@n take a number as input
- *Return: return a positive number
+ * @n: take a number as input
+ * Return: return a positive number
  **/
 
 int _abs(int n)
 {
 	if (n >= 0)
 		return (n);
-	return (-1*n);
+	return (-1 * n);
 }
