8184fbcec7098bb4a25928b447afa6c63aacc6fd
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2022-03-18 08:32:53 -0400

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,9 +6,10 @@
 void print_to_98(int n)
 {
 	int i;
+
 	if (n <= 98)
 	{
-		for (i = n; i <=98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
 				printf("%d, ", i);
@@ -18,12 +19,12 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (i = n; i >=98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			if (i != 98)
 				printf("%d, ", i);
 			else if (i == 98)
 				printf("%d\n", i);
 		}
-	}	
+	}
 }
