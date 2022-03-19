04cb2eae0be1e5591179e320f6df304c69c1fe3a
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2022-03-19 02:37:55 -0400

    1-isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+/**
+ * _isdigit - check whether it is digit or not
+ * @c: accept input
+ * Return: return 1 if digit otherwise 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
